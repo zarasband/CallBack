02dd7c1f7a2758fe70ec72eede640d02f3231899
farshad <zarasband@gmail.com>
2019-10-10 16:59:16 +0330

    first

---
+++ app.js
@@ -0,0 +1,10 @@
+let doHomeWork = (subject, callback) => {
+    console.log('Starting my ${subject} homework');
+    callback();
+};
+
+let finished = () => {
+    console.log('Finished homework');
+};
+
+console.log('peransa');
